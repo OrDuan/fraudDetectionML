8a66cb9ead42d264a662fc52c0459f20a84a2971
or <orduani@gmail.com>
2016-07-30 23:53:23 +0300

    migrated to OO approach

--- data.py
+++ data.py
@@ -1,15 +1,10 @@
 import random
 
-from fixtures import USERS_FIXTURE
-
-
-def get_data():
-    """
-    I don't want to keep
-    """
-    return USERS_FIXTURE
 
 def generate_data():
+    """
+    Generate random data about the users, only for development.
+    """
     results = []
     for d in xrange(1000):
         age = random.randint(2, 50)
@@ -28,12 +23,12 @@ def generate_data():
                 '24hoursFailed': h24_success/7,
                 '30daysFailed': random.randint(h24_success/2, h24_success+age/2),
             },
-            # 'browser': {
-            #     'userAgents': [
-            #         'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36',
-            #         'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.1 Safari/537.36'
-            #     ],
-            # },
+            'browser': {
+                'userAgents': [
+                    'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36',
+                    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.1 Safari/537.36'
+                ],
+            },
         }
         results.append(data)
     return results
@@ -47,18 +42,3 @@ def generate_email():
     domains = ['gmail.com', 'yahoo.com', 'walla.co.il', 'aol.com', 'outlook.com']
     return '{first_name}@{domain}'.format(first_name=random.choice(first_names), domain=random.choice(domains))
 
-
-def parse_to_flat_features(data):
-    """
-    For now I use only 2 features, can add more easly.
-    """
-    results = []
-    for item in data:
-        results.append([
-            item['user']['age'],
-            item['loginHistory']['30daysSuccess'],
-        ])
-    return results
-
-if __name__ == '__main__':
-    print list(get_data())
--- run.py
+++ run.py
@@ -2,41 +2,78 @@ import matplotlib.pyplot as plt
 import numpy as np
 from sklearn import svm
 
-from data import get_data, parse_to_flat_features
-
-plt.figure(dpi=190)
-
-data = get_data()
-faltted = parse_to_flat_features(data)
-
-plt.plot([x[0] for x in faltted], [x[1] for x in faltted], 'bo')
-
-xx, yy = np.meshgrid(np.linspace(-5, 250, 500), np.linspace(-5, 250, 500))
-
-# Generate train data
-X_train = np.array(faltted)
-X_outliers = np.array([[10, 1]])
-
-# fit the model
-clf = svm.OneClassSVM(nu=0.1, gamma=0.1, tol=2)
-clf.fit(X_train)
-for i in range(2, 30):
-    for j in range(2, 10):
-        predict = clf.predict([[i, j]])
-        shape = 'vr' if predict[0] == -1 else 'vg'
-        plt.plot(i, j, shape)
+from fixtures import USERS_FIXTURE
 
 
-# plot the line, the points, and the nearest vectors to the plane
-Z = clf.decision_function(np.c_[xx.ravel(), yy.ravel()])
-Z = Z.reshape(xx.shape)
+class FraudDetector(object):
+    def __init__(self, dpi, nu=0.1, gamma=0.1, tol=2):
+        self.fig = plt.figure(dpi=dpi)
 
-plt.contourf(xx, yy, Z, levels=np.linspace(Z.min(), 0, 10), cmap=plt.cm.Blues_r)
-plt.contour(xx, yy, Z, levels=[0], linewidths=2, colors='red')
-plt.contourf(xx, yy, Z, levels=[0, Z.max()], colors='orange')
+        self.get_data()
+        self.features = []
+        self.generate_features_list()
+        self.clf = svm.OneClassSVM(nu=nu, gamma=gamma, tol=tol)
+        plt.xlim((-5, 60))
+        plt.ylim((-5, 20))
+        plt.title("Find The Outliers")
 
-plt.xlim((-5, 60))
-plt.ylim((-5, 20))
-plt.title("Find the outliers")
+    def run(self):
+        self.train()
+        self.plot_decision_function()
+        self.plot_train()
+        self.plot_predicts()
+        self.show()
 
-plt.show()
+    def generate_features_list(self):
+        """
+        For now I use only 2 features, can add more easly.
+        """
+        results = []
+        for item in self.data:
+            self.features.append([
+                item['user']['age'],
+                item['loginHistory']['30daysSuccess'],
+            ])
+        return results
+
+    def get_data(self):
+        """
+        Using some fixture data, should be override with real data
+        """
+        self.data = USERS_FIXTURE
+
+    def train(self):
+        self.clf.fit(self.features)
+
+    def plot_predicts(self):
+        """
+        Predict some testing data and plot it as triangles.
+        """
+        # fit the model
+        for i in range(2, 30):
+            for j in range(2, 10):
+                predict = self.clf.predict([[i, j]])
+                shape = 'vr' if predict[0] == -1 else 'vg'
+                plt.plot(i, j, shape)
+
+    def plot_train(self):
+
+        plt.plot([x[0] for x in self.features], [x[1] for x in self.features], 'bo')
+
+    def plot_decision_function(self):
+        xx, yy = np.meshgrid(np.linspace(-5, 250, 500), np.linspace(-5, 250, 500))
+
+        # plot the line, the points, and the nearest vectors to the plane
+        Z = self.clf.decision_function(np.c_[xx.ravel(), yy.ravel()])
+        Z = Z.reshape(xx.shape)
+
+        plt.contourf(xx, yy, Z, levels=np.linspace(Z.min(), 0, 10), cmap=plt.cm.Blues_r)
+        plt.contour(xx, yy, Z, levels=[0], linewidths=2, colors='red')
+        plt.contourf(xx, yy, Z, levels=[0, Z.max()], colors='orange')
+
+    def show(self):
+        plt.show()
+
+if __name__ == '__main__':
+    fd = FraudDetector(190)
+    fd.run()
