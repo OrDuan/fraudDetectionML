97b014465ea3136c9fd1fc5ace54d71e993bb388
or <orduani@gmail.com>
2016-07-31 10:55:05 +0300

    init the data on the run method and not on the init. added doc

--- run.py
+++ run.py
@@ -8,16 +8,21 @@ from fixtures import USERS_FIXTURE
 class FraudDetector(object):
     def __init__(self, dpi, nu=0.1, gamma=0.1, tol=2):
         self.fig = plt.figure(dpi=dpi)
-
-        self.get_data()
+        self.data = {}
         self.features = []
         self.generate_features_list()
+
         self.clf = svm.OneClassSVM(nu=nu, gamma=gamma, tol=tol)
+
         plt.xlim((-5, 60))
         plt.ylim((-5, 20))
         plt.title("Find The Outliers")
 
     def run(self):
+        """
+        Get the data, train, predict and plot everything
+        """
+        self.get_data()
         self.train()
         self.plot_decision_function()
         self.plot_train()
@@ -43,13 +48,15 @@ class FraudDetector(object):
         self.data = USERS_FIXTURE
 
     def train(self):
+        """
+        Fit the data to our classifier
+        """
         self.clf.fit(self.features)
 
     def plot_predicts(self):
         """
         Predict some testing data and plot it as triangles.
         """
-        # fit the model
         for i in range(2, 30):
             for j in range(2, 10):
                 predict = self.clf.predict([[i, j]])
@@ -57,10 +64,15 @@ class FraudDetector(object):
                 plt.plot(i, j, shape)
 
     def plot_train(self):
-
+        """
+        Plot the train data(our features)
+        """
         plt.plot([x[0] for x in self.features], [x[1] for x in self.features], 'bo')
 
     def plot_decision_function(self):
+        """
+        Plot the orange area that represent the decision boundary
+        """
         xx, yy = np.meshgrid(np.linspace(-5, 250, 500), np.linspace(-5, 250, 500))
 
         # plot the line, the points, and the nearest vectors to the plane
@@ -72,6 +84,9 @@ class FraudDetector(object):
         plt.contourf(xx, yy, Z, levels=[0, Z.max()], colors='orange')
 
     def show(self):
+        """
+        Show the graph
+        """
         plt.show()
 
 if __name__ == '__main__':
